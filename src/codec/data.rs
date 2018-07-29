68954eae242c8be9af969676e272dce9df451e24
Pierre Zemb <contact@pierrezemb.fr>
2018-07-29 22:48:40 +0200

    fix: data parsing is almost working

--- src/codec/data.rs
+++ src/codec/data.rs
@@ -1,33 +1,56 @@
 use bytes::BytesMut;
 use nom::*;
 use std::io;
+use std::str;
 use tokio_codec::Decoder;
 
 #[derive(Debug)]
 pub struct DataPacket {
-    version: i16,
+    version: u16,
     crc: u32,
     timestamp: u32,
     state: i16,
-    // hostname: String,
-    // service: String,
-    // plugin_output: String,
+    hostname: String,
+    service: String,
+    plugin_output: String,
+}
+
+impl DataPacket {
+    fn new(
+        version: u16,
+        crc: u32,
+        timestamp: u32,
+        state: i16,
+        hostname: &[u8],
+        service: &[u8],
+        plugin_output: &[u8],
+    ) -> DataPacket {
+        DataPacket {
+            version: version,
+            crc: crc,
+            timestamp: timestamp,
+            state: state,
+            hostname: str::from_utf8(hostname).unwrap().to_string(),
+            service: str::from_utf8(service).unwrap().to_string(),
+            plugin_output: str::from_utf8(plugin_output).unwrap().to_string(),
+        }
+    }
 }
 
 // taken from https://github.com/aerostitch/nscatools/blob/master/datapacket.go
 named!(parse_data<&[u8], DataPacket>,do_parse!(
-
     // // parsing binary format
-    version: be_i16 >>
+    version: be_u16 >>
+    take!(2) >>
     crc: be_u32 >>
     timestamp: be_u32 >>
     state: be_i16 >>
-    hostname: take_until_and_consume!("\x00") >>
-    service: take_until_and_consume!("\x00") >>
+    hostname: take!(64) >> // hostname: take_until_and_consume!("\x00") >>
+    service: take!(128) >>
     plugin_output: take_until_and_consume!("\x00") >>
 
     // constructing DataPacket
-    (DataPacket {version, crc, timestamp, state})
+    (DataPacket::new(version, crc, timestamp, state, hostname, service, plugin_output))
   )
 );
 
